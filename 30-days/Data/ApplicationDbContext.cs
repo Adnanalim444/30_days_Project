4d8b973cd7b45e1c9f718f304cce1f0c7abe4088
adnanalim444 <adnanalim444@gmail.com>
2024-01-28 05:28:26 -0800

    Add project files.

---
+++ 30-days/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using _30_days.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace _30_days.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories{ get; set; }
+    }
+        
+}
